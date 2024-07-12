1c6ee61fc050d70b487a6ad0f94e13844f13763f
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 12:27:16 +0100

    Update mappings.go

--- pkg/v1/api/webboot/mappings.go
+++ pkg/v1/api/webboot/mappings.go
@@ -8,21 +8,40 @@ import (
 	cls "github.com/tdrip/apiclient/pkg/v1/client"
 	utils "github.com/tdrip/apiclient/pkg/v1/utils"
 	client "github.com/tdrip/cristie-va-api/pkg/v1/client"
-	models "github.com/tdrip/cristie-va-api/pkg/v1/models"
-	orch "github.com/tdrip/cristie-va-api/pkg/v1/models/orchestration"
+	wb "github.com/tdrip/cristie-va-api/pkg/v1/models/webboot"
 )
 
 const (
 	UriWebbootMap = "v1/webboot/map/"
 )
 
-func CreateDHCPMapping(crs *cls.Client, product string) (models.Event, error) {
-	result := models.Event{}
+func CreateDHCPMapping(crs *cls.Client, product string, os string, mac string, buuid string) (wb.Mappings, error) {
+	result := wb.Mappings{}
+	if !crs.Session.HasToken() {
+		return result, errors.New("webboot DHCP mapping failed - token missing session")
+	}
+
+	request := wb.Mapping{}
+	request.DHCP = true
+	request.InsecureTls = false
+	request.ConfigId = nil
+	request.Mac = mac
+	request.BiosUuid = buuid
+	request.Ip = "DHCP"
+	request.Netmask = "DHCP"
+	request.Gateway = "DHCP"
+	request.DnsAddrs = ""
+	request.OS = os
+	request.Product = product
+	return CreateMapping(crs, request)
+}
+
+func CreateMapping(crs *cls.Client, request wb.Mapping) (wb.Mappings, error) {
+	result := wb.Mappings{}
 	if !crs.Session.HasToken() {
 		return result, errors.New("webboot mapping failed - token missing session")
 	}
 
-	request := orch.Job{Name: product}
 	bytes, res, err := crs.Session.PostBody(UriWebbootMap, &request)
 
 	if err == nil && res != nil && utils.RequestIsSuccessful(res.StatusCode) {
