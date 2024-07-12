47ef8fdd862cf88fd6013b3680042811dd73c377
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 14:56:40 +0100

    added consts

---
+++ pkg/v1/consts/consts.go
@@ -0,0 +1,13 @@
+package consts
+
+// source targets
+const(
+	SourceTarget_Type_RMBR="RBMR"
+	SourceTarget_VMHostType_Standard="STANDARD"
+)
+
+// backup types
+const(
+	Source_BackupType_FileBased="FILE_BASED" // Fileset
+	Source_BackupType_BlockBased="BLOCK_BASED" // Volume Group
+)
