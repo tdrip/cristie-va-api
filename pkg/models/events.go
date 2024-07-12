03ea608dbe9672dc853bdc623a4a7fbc89b82215
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 02:45:15 +0100

    Update events.go

--- pkg/models/events.go
+++ pkg/models/events.go
@@ -5,31 +5,31 @@ import (
 )
 
 type Event struct {
-	Abortable         bool      `json:"abortable,omitempty"`
-	BlockId           int32     `json:"blockId,omitempty"`
-	Completed         bool      `json:"completed,omitempty"`
-	Details           string    `json:"details,omitempty"`
-	Error             int32     `json:"error,omitempty"`
-	Failed            int32     `json:"failed,omitempty"`
-	FinishTime        time.Time `json:"finishTime,omitempty"`
-	Finish_Time       time.Time `json:"finish_time,omitempty"`
-	Id                int32     `json:"id,omitempty"`
-	Identifier        string    `json:"identifier,omitempty"`
-	Logged            bool      `json:"logged,omitempty"`
-	ModifiedTimestamp int64     `json:"modifiedTimestamp,omitempty"`
-	NodeUuid          string    `json:"nodeUuid,omitempty"`
-	Percent           int32     `json:"percent,omitempty"`
-	Replayable        bool      `json:"replayable,omitempty"`
-	Result            string    `json:"result,omitempty"`
-	Schedule          string    `json:"schedule,omitempty"`
-	ScheduleReference string    `json:"scheduleReference,omitempty"`
-	Source            string    `json:"source,omitempty"`
-	StartTime         time.Time `json:"startTime,omitempty"`
-	Start_Time        time.Time `json:"start_time,omitempty"`
-	Status            string    `json:"status,omitempty"`
-	Target            string    `json:"target,omitempty"`
-	Task              string    `json:"task,omitempty"`
-	Type              int32     `json:"type,omitempty"`
-	User              string    `json:"user,omitempty"`
-	Uuid              string    `json:"uuid,omitempty"`
+	Abortable         bool      `json:"abortable,omitempty"`         // false
+	BlockId           int32     `json:"blockId,omitempty"`           // 0
+	Completed         bool      `json:"completed,omitempty"`         // false
+	Details           string    `json:"details,omitempty"`           // Details of what is currently happening - "Something was created"
+	Error             int32     `json:"error,omitempty"`             // 0
+	Failed            int32     `json:"failed,omitempty"`            // 0
+	FinishTime        time.Time `json:"finishTime,omitempty"`        // "2024-07-01T22:18:14.636481742Z"
+	Finish_Time       time.Time `json:"finish_time,omitempty"`       // "2024-07-01T22:18:14.636481742Z"
+	Id                int32     `json:"id,omitempty"`                // ID of the event - 323
+	Identifier        string    `json:"identifier,omitempty"`        // "ochestration"
+	Logged            bool      `json:"logged,omitempty"`            // false
+	ModifiedTimestamp int64     `json:"modifiedTimestamp,omitempty"` // 0
+	NodeUuid          string    `json:"nodeUuid,omitempty"`          // null
+	Percent           int32     `json:"percent,omitempty"`           // 0
+	Replayable        bool      `json:"replayable,omitempty"`        // false
+	Result            string    `json:"result,omitempty"`            // null
+	Schedule          string    `json:"schedule,omitempty"`          // ""
+	ScheduleReference string    `json:"scheduleReference,omitempty"` // "96"
+	Source            string    `json:"source,omitempty"`            //  Event source (if applicable) - "<>"
+	StartTime         time.Time `json:"startTime,omitempty"`         // "2024-07-01T22:18:14.636481742Z"
+	Start_Time        time.Time `json:"start_time,omitempty"`        // "2024-07-01T22:18:14.636481742Z"
+	Status            string    `json:"status,omitempty"`            // The current status of this event - "Completed"
+	Target            string    `json:"target,omitempty"`            // Event target (if applicable) - ""
+	Task              string    `json:"task,omitempty"`              // The task being performed by this event - "DR Orchestration"
+	Type              int32     `json:"type,omitempty"`              // 1
+	User              string    `json:"user,omitempty"`              // "loggedin:user"
+	Uuid              string    `json:"uuid,omitempty"`              // UUID of the event (deprecated) - "f3456789-e678-e678-e678-f3456789"
 }
