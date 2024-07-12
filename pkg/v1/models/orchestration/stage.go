fc6d3e905c517e6bccf6ad0d2c097de2fb441fc8
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 11:13:37 +0100

    Update stage.go

--- pkg/v1/models/orchestration/stage.go
+++ pkg/v1/models/orchestration/stage.go
@@ -3,8 +3,8 @@ package orchestration
 type Stage struct {
 	Blocks  []Block              `json:"blocks,omitempty"`
 	Enabled bool                 `json:"enabled,omitempty"`
-	Id      int32                `json:"id,omitempty"`
-	JobId   int32                `json:"jobId,omitempty"`
+	Id      int                  `json:"id,omitempty"`
+	JobId   int                  `json:"jobId,omitempty"`
 	Name    string               `json:"name,omitempty"`
 	Status  string               `json:"status,omitempty"`
 }
