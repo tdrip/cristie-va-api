0a86e3fc37550b60f441eea9d90eb43cc701eea0
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 11:14:26 +0100

    Update block.go

--- pkg/v1/models/orchestration/block.go
+++ pkg/v1/models/orchestration/block.go
@@ -5,21 +5,21 @@ import (
 )
 
 type BlockReportConfig struct {
-	BlockId    int32    `json:"blockId,omitempty"`
-	Id         int32    `json:"id,omitempty"`
+	BlockId    int      `json:"blockId,omitempty"`
+	Id         int      `json:"id,omitempty"`
 	Recipients []string `json:"recipients,omitempty"`
 }
 
 type Block struct {
 	Description  string             `json:"description,omitempty"`
 	Finished     time.Time          `json:"finished,omitempty"`
-	Id           int32              `json:"id,omitempty"`
-	JobId        int32              `json:"jobId,omitempty"`
+	Id           int                `json:"id,omitempty"`
+	JobId        int                `json:"jobId,omitempty"`
 	Name         string             `json:"name,omitempty"`
 	ReportConfig *BlockReportConfig `json:"reportConfig,omitempty"`
 	Reusable     bool               `json:"reusable,omitempty"`
 	RunReport    bool               `json:"runReport,omitempty"`
-	StageId      int32              `json:"stageId,omitempty"`
+	StageId      int                `json:"stageId,omitempty"`
 	Started      time.Time          `json:"started,omitempty"`
 	Status       string             `json:"status,omitempty"`
 	Type_        string             `json:"type,omitempty"`
