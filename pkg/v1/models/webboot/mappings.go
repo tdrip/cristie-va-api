a1bc535cfdeaf3675404800f941465203a2d8771
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 12:14:08 +0100

    Create mappings.go

---
+++ pkg/v1/models/webboot/mappings.go
@@ -0,0 +1,15 @@
+package webboot
+
+type Mapping struct {
+	BiosUuid     string   `json:"bios_uuid,omitempty"`
+	ConfigId     int      `json:"configId,omitempty"`
+	DnsAddrs     []string `json:"dns_addrs,omitempty"`
+	DrNetworking bool     `json:"drNetworking,omitempty"`
+	Gateway      string   `json:"gateway,omitempty"`
+	Id           int      `json:"id,omitempty"`
+	Ip           string   `json:"ip,omitempty"`
+	Mac          string   `json:"mac,omitempty"`
+	Netmask      string   `json:"netmask,omitempty"`
+	Os           string   `json:"os,omitempty"`
+	Product      string   `json:"product,omitempty"`
+}
