4376daf2cf0bb7f1e9776283452a6f890cb3b9f8
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 08:03:11 +0100

    Create physicaldisk.go

---
+++ pkg/models/disks/physicaldisk.go
@@ -0,0 +1,13 @@
+package disks
+
+type PhysicalDisk struct {
+	DeviceId int32  `json:"deviceId,omitempty"`
+	Id       int32  `json:"id,omitempty"`
+	Name     string `json:"name,omitempty"`
+	Selected bool   `json:"selected,omitempty"`
+	Size     int64  `json:"size,omitempty"`
+	SizeGB   int32  `json:"sizeGB,omitempty"`
+	SizeKB   int64  `json:"sizeKB,omitempty"`
+	SizeMB   int32  `json:"sizeMB,omitempty"`
+	Valid    bool   `json:"valid,omitempty"`
+}
