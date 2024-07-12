16ab734461d28181d4d11973c2089afb0bf78863
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 10:10:40 +0100

    Update client.go

--- pkg/v1/client/client.go
+++ pkg/v1/client/client.go
@@ -2,13 +2,11 @@ package client
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 
 	cls "github.com/tdrip/apiclient/pkg/v1/client"
 	utils "github.com/tdrip/apiclient/pkg/v1/utils"
-	models "github.com/tdrip/cristie-va-api/pkg/v1/models"
-	orch "github.com/tdrip/cristie-va-api/pkg/v1/models/orchestration"
+	auth "github.com/tdrip/cristie-va-api/pkg/v1/api/auth"
 )
 
 const (
@@ -18,19 +16,40 @@ const (
 )
 
 func Login(crs *cls.Client, userid string, pwd string) error {
-	result := models.Event{}
-	if !crs.Session.HasToken() {
-		return result, errors.New("job creation failed - token missin session")
-	}
-
-	request := orch.Job{Name: userid, FailureOption: userid}
+	request := auth.OAuthRequest{Username: userid, Password: userid}
 	bytes, res, err := crs.Session.PostBody(UriUsersAuthPath, &request)
 
 	if err == nil && res != nil && utils.RequestISsUCCESSFUL(res.StatusCode) {
+		result := auth.OAuthResponse{}
 		err = json.Unmarshal(bytes, &result)
-		return res, err
+		if err != nil {
+			return nil
+		}
+		err := cls.SetAuthToken(crs, result.AccessToken)
+		return err
 	}
 
-	return fmt.Errorf("job creation failed with errors: %v", err)
+	return fmt.Errorf("login failed with errors: %v", err)
+
+}
+
+func CheckSession(crs *cls.Client, usesessionid bool) (auth.OAuthResponse, error) {
+	result := auth.OAuthResponse{}
+	uri := UriUsersAuthSession
+	if usesessionid {
+		uri = UriUsersAuthSessionId
+	}
+	bytes, res, err := crs.Session.Get(uri)
+
+	if err == nil && res != nil && utils.RequestISsUCCESSFUL(res.StatusCode) {
+		err = json.Unmarshal(bytes, &result)
+		if err != nil {
+			return result, nil
+		}
+		err := cls.SetAuthToken(crs, result.AccessToken)
+		return result, err
+	}
+
+	return result, fmt.Errorf("job creation failed with errors: %v", err)
 
 }
