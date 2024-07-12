11da2c4bd7d0655f46d3424b6ceb4d4ce5f9edd0
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 12:25:09 +0100

    Update mappings.go

--- pkg/v1/models/webboot/mappings.go
+++ pkg/v1/models/webboot/mappings.go
@@ -3,10 +3,11 @@ package webboot
 type Mapping struct {
 	BiosUuid     string   `json:"bios_uuid,omitempty"`
 	ConfigId     int      `json:"configId,omitempty"`
-	DnsAddrs     []string `json:"dns_addrs,omitempty"`
+	DnsAddrs     string   `json:"dns_addrs,omitempty"`
 	DrNetworking bool     `json:"drNetworking,omitempty"`
 	Gateway      string   `json:"gateway,omitempty"`
 	Id           int      `json:"id,omitempty"`
+	InsecureTls  bool     `json:"insecure_tls,omitempty"`
 	Ip           string   `json:"ip,omitempty"`
 	Mac          string   `json:"mac,omitempty"`
 	Netmask      string   `json:"netmask,omitempty"`
