6c7639d7359ead5110c1f5ecd72de5e2e8b8f1e3
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-12 12:46:04 +0100

    Create rubrikserver.go

---
+++ pkg/v1/models/backupservers/rubrikserver.go
@@ -0,0 +1,24 @@
+package backupservers
+
+import (
+	"time"
+)
+
+type RubrikServer struct {
+	CloudAddress  string         `json:"cloudAddress,omitempty"`
+	CloudId       int32          `json:"cloudId,omitempty"`
+	CloudUuid     string         `json:"cloudUuid,omitempty"`
+	SmbPassword   string         `json:"smbPassword,omitempty"`
+	SmbUsername   string         `json:"smbUsername,omitempty"`
+	BackupClients []BackupClient `json:"backupClients,omitempty"`
+	Host          string         `json:"host,omitempty"`
+	Id            int32          `json:"id,omitempty"`
+	LastUpdate    time.Time      `json:"lastUpdate,omitempty"`
+	Legacy        bool           `json:"legacy,omitempty"`
+	Name          string         `json:"name,omitempty"`
+	OtpSecret     string         `json:"otpSecret,omitempty"`
+	Password      string         `json:"password,omitempty"`
+	ServerVersion string         `json:"serverVersion,omitempty"`
+	Type          string         `json:"type,omitempty"`
+	UserName      string         `json:"userName,omitempty"`
+}
