965e4566225ea6fb84e65aa5b86c89143ed33b76
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-17 15:17:03 +0100

    pointer to time

--- pkg/v1/models/targets/backupsource.go
+++ pkg/v1/models/targets/backupsource.go
@@ -5,13 +5,13 @@ import (
 )
 
 type BackupSource struct {
-	BackupType   string    `json:"backupType,omitempty"` //missing from swagger
-	Hostname     string    `json:"hostName,omitempty"`   //missing from swagger
-	HostId       string    `json:"hostId,omitempty"`     //missing from swagger
-	Id           int       `json:"id,omitempty"`
-	Name         string    `json:"name,omitempty"` //missing from swagger
-	Pit          bool      `json:"pit,omitempty"`
-	PitTimestamp time.Time `json:"pitTimestamp,omitempty"`
-	ServerId     int       `json:"serverId,omitempty"`
-	Type         string    `json:"type,omitempty"`
+	BackupType   string     `json:"backupType,omitempty"` //missing from swagger
+	Hostname     string     `json:"hostName,omitempty"`   //missing from swagger
+	HostId       string     `json:"hostId,omitempty"`     //missing from swagger
+	Id           int        `json:"id,omitempty"`
+	Name         string     `json:"name,omitempty"` //missing from swagger
+	Pit          bool       `json:"pit,omitempty"`
+	PitTimestamp *time.Time `json:"pitTimestamp,omitempty"`
+	ServerId     int        `json:"serverId,omitempty"`
+	Type         string     `json:"type,omitempty"`
 }
