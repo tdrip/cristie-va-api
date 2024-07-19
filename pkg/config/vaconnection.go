e5901576774f3cb4f602484ecf6e7b06bd6e02fb
tdrip <44380828+tdrip@users.noreply.github.com>
2024-07-19 12:51:51 +0100

    Update vaconnection.go

--- pkg/config/vaconnection.go
+++ pkg/config/vaconnection.go
@@ -1,8 +1,11 @@
 package config
 
 type VAConnection struct {
-	Server string `yaml:"server"`
-	User   string `yaml:"user"`
-	PWord  string `yaml:"pword"`
-	Token  string `yaml:"token"`
+	Server        string `yaml:"server"`
+	User          string `yaml:"user"`
+	PWord         string `yaml:"pword"`
+	Token         string `yaml:"token"`
+	DumpRequest   bool   `yaml:"dumprequests"`
+	DumpResponses bool   `yaml:"dumpresponses"`
+	Debug         bool   `yaml:"debug"`
 }
